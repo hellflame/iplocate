0ee6776c85e86cf16a0dfc603b73a78267657ce3
hellflame <hellflamedly@gmail.com>
2015-10-03 14:36:21 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='iplocate',
-    version='1.0.0',
+    version='1.0.1',
     keywords=('ip', 'ipv4', 'ipv6', 'location', 'ip2location', 'ip to location'),
     description="在终端获取本机或者指定ip地址",
     license="MIT",
@@ -19,7 +19,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7'
     ],
-    platforms="linux, mac os",
+    platforms="any",
     entry_points={
         'console_scripts': [
             'iplocate=iplocate.iplocate:main'
