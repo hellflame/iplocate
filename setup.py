de32fc80cbcf7f7d6b2e05b0752dc1bf9566acc2
hellflame <hellflamedly@gmail.com>
2015-10-04 08:19:26 +0800

    更新说明以及版本号更新

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 # coding=utf8
+from setuptools import setup, find_packages
 __author__ = 'hellflame'
 
-from setuptools import setup, find_packages
 
 setup(
     name='iplocate',
-    version='1.0.1',
+    version='1.0.2',
     keywords=('ip', 'ipv4', 'ipv6', 'location', 'ip2location', 'ip to location'),
     description="在终端获取本机或者指定ip地址",
     license="MIT",
